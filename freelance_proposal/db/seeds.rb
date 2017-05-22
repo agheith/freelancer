e1c7198ddad3f992dcc29fe908e6503f25e00859
agheith <amerghth@gmail.com>
2017-05-22 15:57:34 -0700

    Connected Angular2 with Rails Proposal Microservice

--- freelance_proposal/db/seeds.rb
+++ freelance_proposal/db/seeds.rb
@@ -1,14 +1,12 @@
-# 10.times do |proposal|
-# Proposal.create!(
-#     customer: "Customer #{proposal}",
-#     portfolio_url: 'http://amergheith.me',
-#     tools: "Ruby on Rails, Angular 2, and Postgresql",
-#     estimated_hours: (80 + proposal),
-#     hourly_rate: 120,
-#     weeks_to_complete: 12,
-#     client_email: 'amerghth@gmail.com',
-# )
-#
-# end
+10.times do |proposal|
+Proposal.create!(
+    customer: "Customer #{proposal}",
+    portfolio_url: 'http://amergheith.me',
+    tools: "Ruby on Rails, Angular 2, and Postgresql",
+    estimated_hours: (80 + proposal),
+    hourly_rate: 120,
+    weeks_to_complete: 12,
+    client_email: 'amerghth@gmail.com',
+)
 
-# rails g scaffold Proposal customer:string portfolio_url:string tools:string estimated_hours:decimal hourly_rate:decimal weeks_to_complete:integer client_email:string
+end
